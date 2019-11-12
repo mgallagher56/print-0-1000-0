93ec96ff5ebdb444374dc6dd908852945ed05890
mgallagher56 <marc.gallagherreid11@bathspa.ac.uk>
2019-11-12 21:03:02 +0000

    Tidied layout

--- 0-1000-0/app/js/cage.js
+++ 0-1000-0/app/js/cage.js
@@ -1,58 +1,62 @@
-function moveCage($left, $top, $duration=3000) {
-    $('#cage').animate({left: $left+'px', top: $top+'px', speed: 50000}, {
-        duration: $duration,
-        easing: 'linear'
-    })
+function moveCage ($left, $top, $duration = 3000) {
+  $('#cage').animate({
+    left: $left + 'px',
+    top: $top + 'px',
+    speed: 50000
+  }, {
+    duration: $duration,
+    easing: 'linear'
+  })
 }
 
-let crandDuration = getRandNumber()
-let crandDuration1 = getRandNumber()
-let crandDuration2 = getRandNumber()
-let crandDuration3 = getRandNumber()
-let crandDuration4 = getRandNumber()
-let crandDuration5 = getRandNumber()
-let crandDuration6 = getRandNumber()
-let crandDuration7 = getRandNumber()
-let crandDuration8 = getRandNumber()
-let crandDuration9 = getRandNumber()
-let crandDuration10 = getRandNumber()
+const crandDuration = getRandNumber()
+const crandDuration1 = getRandNumber()
+const crandDuration2 = getRandNumber()
+const crandDuration3 = getRandNumber()
+const crandDuration4 = getRandNumber()
+const crandDuration5 = getRandNumber()
+const crandDuration6 = getRandNumber()
+const crandDuration7 = getRandNumber()
+const crandDuration8 = getRandNumber()
+const crandDuration9 = getRandNumber()
+const crandDuration10 = getRandNumber()
 
-let cswitch1 = crandDuration -100
-let cswitch2 = crandDuration + crandDuration2-100
-let cswitch3 = crandDuration + crandDuration2 + crandDuration3-100
-let cswitch4 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4-100
-let cswitch5 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5-100
-let cswitch6 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6-100
-let cswitch7 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 + crandDuration7-100
-let cswitch8 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 + crandDuration7 + crandDuration8-100
-let cswitch9 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 + crandDuration7 + crandDuration8 + crandDuration9-100
-let cswitch10 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 + crandDuration7 + crandDuration8 + crandDuration9 + crandDuration10-100
+const cswitch1 = crandDuration - 100
+const cswitch2 = crandDuration + crandDuration2 - 100
+const cswitch3 = crandDuration + crandDuration2 + crandDuration3 - 100
+const cswitch4 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 - 100
+const cswitch5 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 - 100
+const cswitch6 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 - 100
+const cswitch7 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 + crandDuration7 - 100
+const cswitch8 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 + crandDuration7 + crandDuration8 - 100
+const cswitch9 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 + crandDuration7 + crandDuration8 + crandDuration9 - 100
+const cswitch10 = crandDuration + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 + crandDuration7 + crandDuration8 + crandDuration9 + crandDuration10 - 100
 
 
 function cageMove() {
-    setTimeout(function () {
+  setTimeout(function() {
 
-        moveCage(randTurn1, 2615, crandDuration)
-        switchSkier2(cswitch1, 'cage')
-        moveCage(randTurn2, 5480, crandDuration1)
-        switchSkier1(cswitch2, 'cage')
-        moveCage(randTurn1, 8095, crandDuration2)
-        switchSkier2(cswitch3, 'cage')
-        moveCage(randTurn2, 10810, crandDuration3)
-        switchSkier1(cswitch4, 'cage')
-        moveCage(randTurn1, 13625, crandDuration4)
-        switchSkier2(cswitch5, 'cage')
-        moveCage(randTurn2,16440, crandDuration5)
-        switchSkier1(cswitch6, 'cage')
-        moveCage(randTurn1,19255, crandDuration6)
-        switchSkier2(cswitch7, 'cage')
-        moveCage(randTurn2,21870, crandDuration7)
-        switchSkier1(cswitch8, 'cage')
-        moveCage(randTurn1,24685, crandDuration8)
-        switchSkier2(cswitch9, 'cage')
-        moveCage(randTurn2,27600, crandDuration9)
-        switchSkier1(cswitch10, 'cage')
-        moveCage(950,30000, crandDuration10)
+    moveCage(randTurn1, 2615, crandDuration)
+    switchSkier2(cswitch1, 'cage')
+    moveCage(randTurn2, 5480, crandDuration1)
+    switchSkier1(cswitch2, 'cage')
+    moveCage(randTurn1, 8095, crandDuration2)
+    switchSkier2(cswitch3, 'cage')
+    moveCage(randTurn2, 10810, crandDuration3)
+    switchSkier1(cswitch4, 'cage')
+    moveCage(randTurn1, 13625, crandDuration4)
+    switchSkier2(cswitch5, 'cage')
+    moveCage(randTurn2, 16440, crandDuration5)
+    switchSkier1(cswitch6, 'cage')
+    moveCage(randTurn1, 19255, crandDuration6)
+    switchSkier2(cswitch7, 'cage')
+    moveCage(randTurn2, 21870, crandDuration7)
+    switchSkier1(cswitch8, 'cage')
+    moveCage(randTurn1, 24685, crandDuration8)
+    switchSkier2(cswitch9, 'cage')
+    moveCage(randTurn2, 27600, crandDuration9)
+    switchSkier1(cswitch10, 'cage')
+    moveCage(950, 30000, crandDuration10)
 
-    }, 4400)
+  }, 4400)
 }
--- 0-1000-0/app/js/main.js
+++ 0-1000-0/app/js/main.js
@@ -1,116 +1,138 @@
-function getRandNumber($max = 3500, $min=3200) {
-    return Math.floor(Math.random() * ($max - $min + 1) ) + $min
+function getRandNumber($max = 3500, $min = 3200) {
+  return Math.floor(Math.random() * ($max - $min + 1)) + $min
 }
 
-let randTurn1 = getRandNumber(1150,1050)
-let randTurn2 = getRandNumber(-150,-50)
+let randTurn1 = getRandNumber(1150, 1050)
+let randTurn2 = getRandNumber(-150, -50)
 
 function switchSkier1($timeout, $characterString) {
-    setTimeout(function () {
-        document.getElementById($characterString).className = 'skier'
-    }, $timeout)
+  setTimeout(function () {
+    document.getElementById($characterString).className = 'skier'
+  }, $timeout)
 }
+
 function switchSkier2($timeout, $characterString) {
-    setTimeout(function () {
-        document.getElementById($characterString).className = 'skierSwitch'
-    }, $timeout)
+  setTimeout(function() {
+    document.getElementById($characterString).className = 'skierSwitch'
+  }, $timeout)
 }
 
 function getMarcTotalTime() {
-    return parseInt(randDuration + randDuration1 + randDuration2 + randDuration3 + randDuration4 + randDuration5 + randDuration6 + randDuration7 + randDuration8 + randDuration9 + randDuration10)
+  return parseInt(randDuration + randDuration1 + randDuration2 + randDuration3 + randDuration4 + randDuration5 + randDuration6 + randDuration7 + randDuration8 + randDuration9 + randDuration10)
 }
+
 function getCageTotalTime() {
-    return parseInt(crandDuration + crandDuration1 + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 + crandDuration7 + crandDuration8 + crandDuration9 + crandDuration10)
+  return parseInt(crandDuration + crandDuration1 + crandDuration2 + crandDuration3 + crandDuration4 + crandDuration5 + crandDuration6 + crandDuration7 + crandDuration8 + crandDuration9 + crandDuration10)
 }
+
 function getVaderTotalTime() {
-    return parseInt(vrandDuration + vrandDuration1 + vrandDuration2 + vrandDuration3 + vrandDuration4 + vrandDuration5 + vrandDuration6 + vrandDuration7 + vrandDuration8 + vrandDuration9 + vrandDuration10)
+  return parseInt(vrandDuration + vrandDuration1 + vrandDuration2 + vrandDuration3 + vrandDuration4 + vrandDuration5 + vrandDuration6 + vrandDuration7 + vrandDuration8 + vrandDuration9 + vrandDuration10)
 }
+
 function getKermitTotalTime() {
-    return parseInt(krandDuration + krandDuration1 + krandDuration2 + krandDuration3 + krandDuration4 + krandDuration5 + krandDuration6 + krandDuration7 + krandDuration8 + krandDuration9 + krandDuration10)
+  return parseInt(krandDuration + krandDuration1 + krandDuration2 + krandDuration3 + krandDuration4 + krandDuration5 + krandDuration6 + krandDuration7 + krandDuration8 + krandDuration9 + krandDuration10)
 }
 
-document.getElementById('button').addEventListener('click',function (e) {
-    e.preventDefault()
-    document.getElementById('button').className = 'hidden'
-    document.body.className = 'skiingbg'
-    document.getElementById('layer').className = 'skiing'
-    document.getElementById('text').className = 'hidden'
-    document.getElementById('skier').className = 'skier'
-    document.getElementById('vader').className = 'skier'
-    document.getElementById('cage').className = 'skier'
-    document.getElementById('kermit').className = 'skier'
-    document.getElementById('finish').className = 'finish'
-    document.getElementById("mario").play();
-    setTimeout(function () {
-        document.getElementById("race").play();
-    },4000)
-    setTimeout(function () {
-        $("#skier, #vader, #kermit, #cage").stop()
-        document.getElementById('finish').className = 'hidden'
-        document.getElementById('output').className = 'hidden'
-        document.getElementById('banner').className = 'banner'
-        document.body.className = 'podium'
-        document.getElementById("cheer").play();
-    }, 41100)
+document.getElementById('button').addEventListener('click', function(e) {
+  e.preventDefault()
+  document.getElementById('button').className = 'hidden'
+  document.body.className = 'skiingbg'
+  document.getElementById('layer').className = 'skiing'
+  document.getElementById('text').className = 'hidden'
+  document.getElementById('skier').className = 'skier'
+  document.getElementById('vader').className = 'skier'
+  document.getElementById('cage').className = 'skier'
+  document.getElementById('kermit').className = 'skier'
+  document.getElementById('finish').className = 'finish'
+  document.getElementById("mario").play();
+  setTimeout(function() {
+    document.getElementById("race").play();
+  }, 4000)
+  setTimeout(function() {
+    $("#skier, #vader, #kermit, #cage").stop()
+    document.getElementById('finish').className = 'hidden'
+    document.getElementById('output').className = 'hidden'
+    document.getElementById('banner').className = 'banner'
+    document.body.className = 'podium'
+    document.getElementById("cheer").play();
+  }, 41100)
 
-    let marcTime = getMarcTotalTime()
-    let cageTime= getCageTotalTime()
-    let kermitTime = getKermitTotalTime()
-    let vaderTime = getVaderTotalTime()
+  let marcTime = getMarcTotalTime()
+  let cageTime = getCageTotalTime()
+  let kermitTime = getKermitTotalTime()
+  let vaderTime = getVaderTotalTime()
 
-    function decideWinner(){
-        if (marcTime < cageTime && marcTime < kermitTime && marcTime < vaderTime){
-            setTimeout(function () {
-                $("audio#marcWin")[0].play()
-                $("#vader, #kermit, #cage").removeClass('skier')
-                $("#vader, #kermit, #cage").addClass('hidden')
-                document.getElementById("banner").innerHTML = "<h1>Marc is the champ!</h1>";
-                $('#skier').animate({left: 400, top:0, speed: 1000})
-                document.getElementById('skier').className = 'skier2'
-            },41300)
-            return marcTime
-        } else if (cageTime < marcTime && cageTime < kermitTime && cageTime < vaderTime){
-            setTimeout(function () {
-                $("audio#cageWin")[0].play()
-                $("#vader, #kermit, #skier").removeClass('skier')
-                $("#vader, #kermit, #skier").addClass('hidden')
-                document.getElementById("banner").innerHTML = "<h1>Nic Cage is the champ!</h1>";
-                $('#cage').animate({left: 400, top:0, speed: 1000})
-                document.getElementById('cage').className = 'skier2'
-            },41300)
-            return cageTime
-        } else if (kermitTime < marcTime && kermitTime < cageTime && kermitTime < vaderTime){
-            setTimeout(function () {
-                $("audio#kermitWin")[0].play()
-                $("#vader, #skier, #cage").removeClass('skier')
-                $("#vader, #skier, #cage").addClass('hidden')
-                document.getElementById("banner").innerHTML = "<h1>Kermit is the champ!</h1>";
-                $('#kermit').animate({left: 400, top:0, speed: 1000})
-                document.getElementById('kermit').className = 'skier2'
-            },41300)
-            return kermitTime
-        } else {
-            setTimeout(function () {
-                $("audio#vaderWin")[0].play()
-                $("#skier, #kermit, #cage").removeClass('skier')
-                $("#skier, #kermit, #cage").addClass('hidden')
-                document.getElementById("banner").innerHTML = "<h1>Darth Vader is the champ!</h1>";
-                $('#vader').animate({left: 400, top:0, speed: 1000})
-                document.getElementById('vader').className = 'skier2'
-            },41300)
-            return vaderTime
-        }
+  function decideWinner() {
+    if (marcTime < cageTime && marcTime < kermitTime && marcTime < vaderTime) {
+      setTimeout(function() {
+        $("audio#marcWin")[0].play()
+        $("#vader, #kermit, #cage").removeClass('skier')
+        $("#vader, #kermit, #cage").addClass('hidden')
+        document.getElementById("banner").innerHTML = "<h1>Marc is the champ!</h1>";
+        $('#skier').animate({
+          left: 400,
+          top: 0,
+          speed: 1000
+        })
+        document.getElementById('skier').className = 'skier2'
+      }, 41300)
+      return marcTime
+    } else if (cageTime < marcTime && cageTime < kermitTime && cageTime < vaderTime) {
+      setTimeout(function() {
+        $("audio#cageWin")[0].play()
+        $("#vader, #kermit, #skier").removeClass('skier')
+        $("#vader, #kermit, #skier").addClass('hidden')
+        document.getElementById("banner").innerHTML = "<h1>Nic Cage is the champ!</h1>";
+        $('#cage').animate({
+          left: 400,
+          top: 0,
+          speed: 1000
+        })
+        document.getElementById('cage').className = 'skier2'
+      }, 41300)
+      return cageTime
+    } else if (kermitTime < marcTime && kermitTime < cageTime && kermitTime < vaderTime) {
+      setTimeout(function() {
+        $("audio#kermitWin")[0].play()
+        $("#vader, #skier, #cage").removeClass('skier')
+        $("#vader, #skier, #cage").addClass('hidden')
+        document.getElementById("banner").innerHTML = "<h1>Kermit is the champ!</h1>";
+        $('#kermit').animate({
+          left: 400,
+          top: 0,
+          speed: 1000
+        })
+        document.getElementById('kermit').className = 'skier2'
+      }, 41300)
+      return kermitTime
+    } else {
+      setTimeout(function() {
+        $("audio#vaderWin")[0].play()
+        $("#skier, #kermit, #cage").removeClass('skier')
+        $("#skier, #kermit, #cage").addClass('hidden')
+        document.getElementById("banner").innerHTML = "<h1>Darth Vader is the champ!</h1>";
+        $('#vader').animate({
+          left: 400,
+          top: 0,
+          speed: 1000
+        })
+        document.getElementById('vader').className = 'skier2'
+      }, 41300)
+      return vaderTime
     }
-    let totalScrollTime = decideWinner()
-    function setScrollTime($totalTime) {
-        setTimeout(function () {
-            $('html, body').animate({scrollTop: $(document).height()}, $totalTime, 'linear')
-        }, 4600)
-    }
-    setScrollTime(totalScrollTime)
-    marcMove()
-    vaderMove()
-    cageMove()
-    kermitMove()
+  }
+  let totalScrollTime = decideWinner()
+
+  function setScrollTime($totalTime) {
+    setTimeout(function() {
+      $('html, body').animate({
+        scrollTop: $(document).height()
+      }, $totalTime, 'linear')
+    }, 4600)
+  }
+  setScrollTime(totalScrollTime)
+  marcMove()
+  vaderMove()
+  cageMove()
+  kermitMove()
 })
-
